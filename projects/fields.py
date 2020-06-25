1b6a296a45103897b9b1b70a32eb7f8cee762416
Andrew <andrew.c.hawes@gmail.com>
2020-06-24 23:21:18 -0500

    Add custom ModelMultipleChoiceField

---
+++ projects/fields.py
@@ -0,0 +1,141 @@
+from django.core.exceptions import ValidationError
+from django.forms.fields import ChoiceField, Field
+from django.forms.models import ModelChoiceIterator, ModelChoiceField
+from django.forms.widgets import (
+    MultipleHiddenInput, SelectMultiple,
+)
+from django.utils.translation import gettext_lazy as _
+
+
+class MyModelChoiceIterator(ModelChoiceIterator):
+
+    def __iter__(self):
+        if self.field.empty_label is not None:
+            yield ("", self.field.empty_label)
+        queryset = self.queryset
+
+        for obj in queryset:
+            yield self.choice(obj)
+
+
+class MyModelChoiceField(ModelChoiceField):
+    # Disabled lines are surrounded by triple quotes, as this modification
+    # may allow a bug to resurface.
+
+    iterator = ModelChoiceIterator
+    def __init__(self, queryset, *, empty_label="---------",
+                 required=True, widget=None, label=None, initial=None,
+                 help_text='', to_field_name=None, limit_choices_to=None,
+                 **kwargs):
+        super().__init__(queryset, empty_label=empty_label, required=required,
+                         widget=widget, label=label, initial=initial,
+                         help_text=help_text, to_field_name=to_field_name,
+                         limit_choices_to=limit_choices_to, **kwargs)
+
+    def __deepcopy__(self, memo):
+        result = super(ChoiceField, self).__deepcopy__(memo)
+        """
+        # Need to force a new ModelChoiceIterator to be created, bug #11183
+        if self.queryset is not None:
+            result.queryset = self.queryset.all()
+        """
+        return result
+
+
+class MyModelMultipleChoiceField(MyModelChoiceField):
+    """A MultipleChoiceField whose choices are a model QuerySet."""
+    widget = SelectMultiple
+    hidden_widget = MultipleHiddenInput
+    default_error_messages = {
+        'list': _('Enter a list of values.'),
+        'invalid_choice': _('Select a valid choice. %(value)s is not one of the'
+                            ' available choices.'),
+        'invalid_pk_value': _('“%(pk)s” is not a valid value.')
+    }
+
+    def __init__(self, queryset, **kwargs):
+        super().__init__(queryset, empty_label=None, **kwargs)
+
+    def to_python(self, value):
+        if not value:
+            return []
+        return list(self._check_values(value))
+
+    def clean(self, value):
+        value = self.prepare_value(value)
+        if self.required and not value:
+            raise ValidationError(self.error_messages['required'], code='required')
+        elif not self.required and not value:
+            return self.queryset.none()
+        if not isinstance(value, (list, tuple)):
+            raise ValidationError(self.error_messages['list'], code='list')
+        qs = self._check_values(value)
+        # Since this overrides the inherited ModelChoiceField.clean
+        # we run custom validators here
+        self.run_validators(value)
+        return qs
+
+    def _check_values(self, value):
+        """
+        Given a list of possible PK values, return a QuerySet of the
+        corresponding objects. Raise a ValidationError if a given value is
+        invalid (not a valid PK, not in the queryset, etc.)
+        """
+        key = self.to_field_name or 'pk'
+        # deduplicate given values to avoid creating many querysets or
+        # requiring the database backend deduplicate efficiently.
+        try:
+            value = frozenset(value)
+        except TypeError:
+            # list of lists isn't hashable, for example
+            raise ValidationError(
+                self.error_messages['list'],
+                code='list',
+            )
+        for pk in value:
+            try:
+                self.queryset.filter(**{key: pk})
+            except (ValueError, TypeError):
+                raise ValidationError(
+                    self.error_messages['invalid_pk_value'],
+                    code='invalid_pk_value',
+                    params={'pk': pk},
+                )
+        qs = self.queryset.filter(**{'%s__in' % key: value})
+        pks = {str(getattr(o, key)) for o in qs}
+        for val in value:
+            if str(val) not in pks:
+                raise ValidationError(
+                    self.error_messages['invalid_choice'],
+                    code='invalid_choice',
+                    params={'value': val},
+                )
+        return qs
+
+    def prepare_value(self, value):
+        if (hasattr(value, '__iter__') and
+                not isinstance(value, str) and
+                not hasattr(value, '_meta')):
+            prepare_value = super().prepare_value
+            return [prepare_value(v) for v in value]
+        return super().prepare_value(value)
+
+    def has_changed(self, initial, data):
+        if self.disabled:
+            return False
+        if initial is None:
+            initial = []
+        if data is None:
+            data = []
+        if len(initial) != len(data):
+            return True
+        initial_set = {str(value) for value in self.prepare_value(initial)}
+        data_set = {str(value) for value in data}
+        return data_set != initial_set
+
+
+def modelform_defines_fields(form_class):
+    return hasattr(form_class, '_meta') and (
+            form_class._meta.fields is not None or
+            form_class._meta.exclude is not None
+    )
